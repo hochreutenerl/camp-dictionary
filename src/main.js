32b556cc7a4e6927b2f7900c73f9124ec87b332a
hochrluc <hochrluc@students.zhaw.ch>
2020-04-19 18:42:40 +0200

    Added PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import i18n from './i18n'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: true
+        }
+    },
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    pwa: {
+        name: "Camp dictionary",
+        themeColor: "#4caf50",
     }
-  },
-  "transpileDependencies": [
-    "vuetify"
-  ]
 };
